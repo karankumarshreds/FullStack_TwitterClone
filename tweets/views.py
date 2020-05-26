fb34ba367a5f8a4ae6d6c8c08b05417eb6ee4cfb
Karan Kumar <nailedITnerd@gmail.com>
2020-05-27 01:14:33 +0530

    Hooked JavaScript with ListView

--- tweets/views.py
+++ tweets/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 from .models import Tweet
 
-# Create your views here.
+#home
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', {})
 
+#list view
+def tweet_list_view(request):
+    try:
+        query_set = Tweet.objects.all()
+        tweet_list = [{"id": query.id, "content": query.content} for query in query_set]
+        print(tweet_list)
+        data = {
+            "response": tweet_list
+        }
+        status = 200
+    except:
+        data = {
+            "response": 'Not Found'
+        }
+        status = 404
+    return JsonResponse(data, status=status)
+    
 
-from django.http import JsonResponse
+#detail view
 def tweet_detail_view(request, id):
     data = {
         "id": id
--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from tweets.views import home_view, tweet_detail_view
+from tweets.views import home_view, tweet_detail_view, tweet_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
+    path('tweets/', tweet_list_view, name="tweets"),
     path('tweets/<int:id>', tweet_detail_view, name="tweets"),
 ]
 
