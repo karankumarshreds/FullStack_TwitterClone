51d77528ee7b19623c1f460f9b6dba2f21cc522f
Karan Kumar <nailedITnerd@gmail.com>
2020-05-27 23:57:48 +0530

    Add tweet create form

--- tweets/views.py
+++ tweets/views.py
@@ -3,10 +3,26 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 
 from .models import Tweet
+from .forms import TweetForm
 
 #home
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', {})
+
+#create_view
+def tweet_create_view(request):
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit=False)
+        obj.save()
+        #to make it empty afer saving
+        form = TweetForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'components/form.html', context)
+
+
 
 #list view
 def tweet_list_view(request):
--- twitter_clone/urls.py
+++ twitter_clone/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.urls import path
 
-from tweets.views import home_view, tweet_detail_view, tweet_list_view
+from tweets.views import (home_view,
+    tweet_detail_view, 
+    tweet_list_view,
+    tweet_create_view,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name="home"),
     path('tweets/', tweet_list_view, name="tweets"),
     path('tweets/<int:id>', tweet_detail_view, name="tweets"),
+    path('create-tweet/', tweet_create_view, name="create-tweet"),
 ]
 
