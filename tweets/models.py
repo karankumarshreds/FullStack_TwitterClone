dab747bdb54103d96a4a54ae2d7c8f101bb4a3f2
Karan Kumar <nailedITnerd@gmail.com>
2020-05-29 21:28:00 +0530

    Add all serialized views

--- tweets/models.py
+++ tweets/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Tweet(models.Model):
     #foreign key means many-to-one 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.ImageField(upload_to="images/", blank=True, null=True)
     
--- tweets/views.py
+++ tweets/views.py
@@ -4,13 +4,51 @@ from django.http import JsonResponse
 
 from .models import Tweet
 from .forms import TweetForm
+from .serializers import TweetSerializer
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 #home
 def home_view(request, *args, **kwargs):
     return render(request, 'pages/home.html', {})
 
-#create_view
+#create_view using serialiers
+@api_view(['POST'])
 def tweet_create_view(request):
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid():
+        #we need the user field as it cannot be null
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
+
+#list view using serializers
+@api_view(['GET'])
+def tweet_list_view(request):
+    tweets = Tweet.objects.all()
+    serializer = TweetSerializer(tweets, many=True)
+    data = {
+            "response": serializer.data
+        }
+    print(serializer)
+    return Response(data, status=200)
+
+#detail view using serializer
+@api_view(['GET'])
+def tweet_detail_view(request, id):
+    tweet = Tweet.objects.get(id=id)
+    serializer = TweetSerializer(tweet)
+    data = {
+        "id": id,
+        "content": serializer.data
+    }
+    return Response(data, status=200)
+    
+
+#create_view without using serializers
+##### NOT BEING USED #####
+def tweet_create_view_(request):
     form = TweetForm(request.POST or None)
     #getting the data from the below input field
     #<input type="hidden" name="next" value="/"/>
@@ -19,6 +57,7 @@ def tweet_create_view(request):
         return JsonResponse({}, status=401)
     if form.is_valid():
         obj = form.save(commit=False)
+        obj.user = request.user 
         obj.save()
         #the way to check if the request is ajax 
         if request.is_ajax():
@@ -33,10 +72,9 @@ def tweet_create_view(request):
     }
     return render(request, 'components/form.html', context)
 
-
-
-#list view
-def tweet_list_view(request):
+#list view without using serializers
+##### NOT BEING USED #####
+def tweet_list_view_(request):
     try:
         query_set = Tweet.objects.all()
         # tweet_list = [
@@ -59,8 +97,9 @@ def tweet_list_view(request):
     return JsonResponse(data, status=status)
     
 
-#detail view
-def tweet_detail_view(request, id):
+#detail view without using serializers 
+##### NOT BEING USED #####
+def tweet_detail_view_(request, id):
     data = {
         "id": id
     }
